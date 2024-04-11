71591541202adec522604fbd2b5c4833276a49b8
vanec06 <vanesacha06@gmail.com>
2024-04-11 18:24:48 -0500

    se modifico la validacion de notificacion y se le agrego calibracion

--- src/validation/validationNotificacion.js
+++ src/validation/validationNotificacion.js
@@ -3,6 +3,6 @@ import { check } from "express-validator";
 export const validatorNotifi = [
     check ('fecha', 'La fecha es requerida y debe tener A-M-D').notEmpty(),
     check ('comentarios', 'es requerido que haga una descripcion o comentario').isLength({ max: 50 }).notEmpty(),
-    check ('tipo_mantenimiento','por favor, seleccione una opcion').isIn(['preventivo', 'correctivo']),
+    check ('tipo_mantenimiento','por favor, seleccione una opcion').isIn(['preventivo', 'correctivo','calibracion']),
     check ('id_maquina','ID, maquina ').isLength({ max: 10 }).notEmpty().isNumeric().withMessage(' numérico'),
 ];
